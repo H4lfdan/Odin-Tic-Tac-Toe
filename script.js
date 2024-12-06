c4e345288f88749c6d9163fbd0f6eac28f3bff23
H4lfdan <danrobnett@gmail.com>
2024-12-06 17:28:24 -0600

    Add user input fields to select cell by row and column. Play Round button changes selected cell value to player token.

--- script.js
+++ script.js
@@ -1,3 +1,95 @@
-const gameBoard = (function() {
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    const placeToken = (row, column, player) => {
+        const targetCell = board[row][column];
+        if(targetCell.getValue() === 0) {
+            targetCell.addToken(player);
+        } else return;
+    };
     
-})
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+            console.log(boardWithCellValues);
+    };
+
+    return { getBoard, placeToken, printBoard };
+}
+
+function Cell() {
+    let value = 0;
+
+    const addToken = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addToken,
+        getValue
+    };
+}
+
+function GameController(
+    playerOneName = "Player One",
+    playerTwoName = "Player Two"
+) {
+    const board = Gameboard();
+    const players = [
+        {
+            name: playerOneName,
+            token: 1
+        },
+        {
+            name: playerTwoName,
+            token: 2
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players [1] : players[0];
+    };
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    };
+
+    const playRound = (row, column) => {
+        console.log(`${getActivePlayer().name} takes cell ${row} by ${column}.`);
+        board.placeToken(row, column, getActivePlayer().token);
+
+        switchPlayerTurn();
+        printNewRound();
+    };
+
+    printNewRound();
+
+    return {
+        playRound,
+        getActivePlayer
+    };
+}
+
+const game = GameController();
+
+function testRound() {
+    const row = document.getElementById("row").value;
+    const column = document.getElementById("column").value;
+    game.playRound(row, column);
+}
